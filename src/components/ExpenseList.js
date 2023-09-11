1d9edb6b6387f21aa15ec559828c8084707fa709
coddingbear <surfwon@naver.com>
2023-09-11 09:24:11 +0900

    예산 계산기 작성 완료

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './ExpenseList.css';
+import ExpenseItem from './ExpenseItem';
+import { MdDelete } from 'react-icons/md';
+
+const ExpenseList = ({expenses, handleDelete, handleEdit, clearItems }) => {
+	// console.log(expenses);
+	
+	return (
+		<>
+			<ul className="list">
+				{/* Expense Item */}
+				{expenses.map((expense) => {
+					return (
+						<ExpenseItem key={expense.id}
+							expense={expense}
+							handleDelete={handleDelete}
+							handleEdit={handleEdit}
+						/>
+					);
+				})}
+			</ul>
+			{expenses.length > 0 && (
+				<button className="btn" onClick={clearItems}>
+					목록 지우기
+					<MdDelete className='btn-icon' />
+				</button>
+			)}
+		</>
+	);	
+}
+
+export default ExpenseList
